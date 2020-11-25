b65add78a759fe1584c3352f242a1e38f1aeedc5
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-11-25 23:30:32 +0100

    Start to process rectangles

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,30 @@
 module Main where
 
+import qualified Data.Map as M
+
 main = do
   lines <- fmap lines getContents
   print $ process lines
 
-process :: [String] -> [[Int]]
-process = map processLine
+process ls = map points rectangles
+  where
+    rectangles = map createRectangle numbers
+    numbers = map parseLine ls
 
-processLine :: String -> [Int]
-processLine = map read . words
+parseLine :: String -> [Int]
+parseLine = map read . words
+
+createRectangle [n, x, y, w, h] = Rectangle {n = n, x1 = x, y1 = y, x2 = x+w-1, y2 = y+h-1}
+
+data Rectangle = Rectangle {
+  n :: Int,
+  x1 :: Int,
+  y1 :: Int,
+  x2 :: Int,
+  y2 :: Int
+} deriving (Show)
+
+points r = do
+  x <- [x1 r..x2 r]
+  y <- [y1 r..y2 r]
+  return (x, y)
