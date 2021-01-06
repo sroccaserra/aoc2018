18e6093cbebb594350b667892e979fa5952db99e
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-01-06 22:53:36 +0100

    [day 04] Group by id

--- src/04.hs
+++ src/04.hs
@@ -1,14 +1,34 @@
 import Data.Char
+import Data.Ord
+import Data.Function
 import Data.List
 import Text.ParserCombinators.ReadP
+
+
+solve xs = grouped
+  where grouped = foldl step [] xs
+        sleepTimesById = groupBy ((==) `on` fst) $ map (\(id, xs) -> (id, sleepTimes xs)) grouped
+        sleepTimeSumById = map (foldl1 (\(id, x) (_, y)-> (id, x+y))) sleepTimesById
+        m = maximumBy (comparing snd) sleepTimeSumById
+
+step xs (Shift id) = (id, []):xs
+step ((id, evs):xs) x = (id, evs++[x]):xs
 
 data Event = Shift Id
            | Asleep Timestamp
            | Awake Timestamp
-           deriving (Show)
+           deriving (Show, Eq)
 
-type Timestamp = (Int, Int)
+type Timestamp = Int
 type Id = Int
+
+sleepTime (Asleep x) (Awake y) = y - x
+
+sleepTimes [] = 0
+sleepTimes (x:y:xs) = (sleepTime x y) + (sleepTimes xs)
+
+---
+-- Parsing and main
 
 parse = map parseLine . sort . lines
 
@@ -17,7 +37,7 @@ parseLine = fst . last . readP_to_S (choice [shift, awake, asleep])
 timestamp :: ReadP Timestamp
 timestamp =
   (munch1 $ not . isSpace) *> string " " *>
-  ((,) <$> (read <$> munch1 isDigit <*string ":") <*> (read <$> munch1 isDigit))
+  (munch1 isDigit *> string ":") *> (read <$> munch1 isDigit)
   <* string "] "
 
 shift = timestamp *> string "Guard #" *> (Shift . read <$> munch1 isDigit)
@@ -26,4 +46,4 @@ asleep = Asleep <$> timestamp <* string "falls asleep"
 
 main = do
   input <- getContents
-  mapM_ print $ map show $ parse input
+  mapM_ print $ solve $ parse input
