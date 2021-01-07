7273ff6e974a2cb153c16dbbf72cc7f24e29f523
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-01-07 22:12:49 +0100

    [day 05] Solve part two

--- src/05.hs
+++ src/05.hs
@@ -1,12 +1,22 @@
 import Data.Char
+import Data.List
 
-simplify :: String -> String
-simplify = foldr step ""
+partOne = length . fullReact
+
+partTwo xs = minimum lengths
+  where letters = sort $ nub $ map toLower $ xs
+        lengths = map (\c -> partOne $ exclude c xs) letters
+
+fullReact :: String -> String
+fullReact = foldr step ""
   where step x (y:ys) | doesReact x y = ys
         step x ys = x:ys
 
 doesReact x y = x /= y && toUpper x == toUpper y
 
+exclude c = filter ((/= c) . toLower)
+
 main = do
   input <- getContents
-  print $ length $ simplify $ head $ lines input
+  print $ partOne $ head $ lines input
+  print $ partTwo $ head $ lines input
