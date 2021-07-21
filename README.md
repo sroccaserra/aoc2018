807bc791058dbe4120ab1162c04cad9356504f13
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-07-21 22:37:20 +0200

    [day 06] Affiche les points sur la grille

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ $ stack build
 ```
 $ awk -f src/03.awk src/03.txt | stack runhaskell -- src/03.hs
 ```
+
+## Learnings
+
+Voir aussi :
+- <https://github.com/sroccaserra/aoc2015#learnings>
+- <https://github.com/sroccaserra/aoc2019#learnings>
+- <https://github.com/sroccaserra/aoc2020#learnings>
+
+### Haskell
+
+- Générer une liste de liste de coordonnées (par exemple `[[(Int, Int)]]`) :
+    - `coordsGrid = map (\y -> (map (flip (,) y) [0..maxX])) [0..maxY]`
+- Appliquer une fonction sur toutes les coordonnées d'une liste de liste :
+    - `map f <$> coordsGrid`
--- src/06.hs
+++ src/06.hs
@@ -1,9 +1,19 @@
 import Data.Char
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
 import Text.ParserCombinators.ReadP
 
-main = interact $ show . partOne . (map parseLine) . lines
+main = do
+  points <- (map parseLine . lines) <$> getContents
+  mapM_ putStrLn $ partOne points
 
-partOne = id
+partOne ps = map namePoint <$> coordsGrid
+  where namePoint p = Map.findWithDefault '.' p pointNames
+        pointNames :: Map (Int, Int) Char
+        pointNames = Map.fromList $ zip ps symbols
+        maxX = maximum $ map fst ps
+        maxY = maximum $ map snd ps
+        coordsGrid = map (\y -> (map (flip (,) y) [0..maxX])) [0..maxY]
 
 parseLine = fst . last . readP_to_S point
 
@@ -11,3 +21,5 @@ point = (,) <$> int <*> (string ", " *> int)
 
 int :: ReadP Int
 int = read <$> munch1 isDigit
+
+symbols = (map chr [97..122]) ++ (map chr [65..90])
