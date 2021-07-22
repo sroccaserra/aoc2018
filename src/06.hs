b6adfb605a4dae63aa43877957cf19a37cc2a684
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-07-22 10:40:40 +0200

    [day 06] Affiche la carte

--- src/06.hs
+++ src/06.hs
@@ -1,4 +1,6 @@
 import Data.Char
+import Data.Ord
+import Data.List
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
 import Text.ParserCombinators.ReadP
@@ -7,13 +9,19 @@ main = do
   points <- (map parseLine . lines) <$> getContents
   mapM_ putStrLn $ partOne points
 
-partOne ps = map namePoint <$> coordsGrid
+partOne ps = map (namePoint . closestPoint ps) <$> gridForPoints ps
   where namePoint p = Map.findWithDefault '.' p pointNames
-        pointNames :: Map (Int, Int) Char
         pointNames = Map.fromList $ zip ps symbols
+
+gridForPoints ps = map (\y -> (map (flip (,) y) [0..maxX])) [0..maxY]
+  where
         maxX = maximum $ map fst ps
         maxY = maximum $ map snd ps
-        coordsGrid = map (\y -> (map (flip (,) y) [0..maxX])) [0..maxY]
+
+closestPoint ps p = fst $ minimumBy (comparing snd) $ map (\x -> (x, distance p x)) ps
+  where coords = gridForPoints ps
+
+distance (x1, y1) (x2, y2) = abs (x2 - x1) + abs (y2 -y1)
 
 parseLine = fst . last . readP_to_S point
 
