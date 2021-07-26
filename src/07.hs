e9df4d60808c72e6f6e23bd8961d50e0cc39e8de
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-07-26 23:38:04 +0200

    [day 07] Build dependencies

--- src/07.hs
+++ src/07.hs
@@ -1,3 +1,5 @@
+import Data.Ix (range)
+import qualified Data.Map as M
 import Text.ParserCombinators.ReadP
 
 import Common (getParsedLines)
@@ -6,7 +8,11 @@ main = do
   input <- getParsedLines 7 parser
   print $ partOne input
 
-partOne = id
+partOne input = m
+  where
+    m = foldl (\acc (a, b) -> M.insertWith (++) a [b] acc) emptyMap input
+    emptyMap = M.fromList $ zip letters (cycle [""])
+    letters = range ('A', 'Z')
 
 parser :: ReadP (Char, Char)
-parser = (,) <$> (string "Step " *> get) <*> (string " must be finished before step " *> get)
+parser = flip (,) <$> (string "Step " *> get) <*> (string " must be finished before step " *> get)
