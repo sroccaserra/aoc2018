209996f77d663d0726b85defcd5a49d5eb8df894
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-11-30 20:47:34 +0100

    r Use deque instead of SimpleQueue
    Because deques are easier to initialize from lists.

--- src/08.py
+++ src/08.py
@@ -1,19 +1,15 @@
 import sys
 import fileinput
 
-from queue import SimpleQueue
+from collections import deque
 
 def solve_part_one(ns):
-    q = SimpleQueue()
-    for n in ns:
-        q.put(n)
+    q = deque(ns)
     return walk_tree_1(q)
 
 
 def solve_part_two(ns):
-    q = SimpleQueue()
-    for n in ns:
-        q.put(n)
+    q = deque(ns)
     return walk_tree_2(q)
 
 
@@ -24,28 +20,28 @@ def solve_part_two(ns):
 
 
 def walk_tree_1(q):
-    c = q.get()
-    n = q.get()
+    c = q.popleft()
+    n = q.popleft()
     s = 0
     for _ in range(c):
         s += walk_tree_1(q)
     for _ in range(n):
-        s += q.get()
+        s += q.popleft()
     return s
 
 
 def walk_tree_2(q):
-    c = q.get()
-    n = q.get()
+    c = q.popleft()
+    n = q.popleft()
     s = 0
     children = []
     for _ in range(c):
         children.append(walk_tree_2(q))
     for i in range(n):
         if c == 0:
-            s += q.get()
+            s += q.popleft()
         else:
-            i = q.get()
+            i = q.popleft()
             if 0 < i and i <= len(children):
                 s += children[i-1]
     return s
@@ -53,6 +49,6 @@ def walk_tree_2(q):
 
 if __name__ == '__main__' and not sys.flags.interactive:
     line = next(fileinput.input())
-    ns = [int(s) for s in line.split()]
+    ns = [int(w) for w in line.split()]
     print(solve_part_one(ns))
     print(solve_part_two(ns))
